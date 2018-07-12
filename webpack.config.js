6821ec211567537569c48a2ed559458b3d52c5dd
andreGarvin <andregarvin718@gmail.com>
2018-07-12 00:44:38 -0400

    updated webpack to user babel-preset-env and webpack pugling to remove the output folder and edited webpack dependencies to follow webpacke setup standards

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,54 @@
-const { resolve } = require('path')
+const { resolve } = require('path');
 
-const entryPath = resolve(`${__dirname}/app/src`)
-const outPath = resolve(`${__dirname}/dist`)
+const webpack = require('webpack'); //to access built-in plugins
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = env => {
-    return {
-        entry: resolve(`${entryPath}/index.js`),
-        output: {
-            path: outPath,
-            filename: 'bundle.js'
-        },
-        module: {
-            loaders: [
-                {
-                    test: /\.js$/,
-                    include: entryPath,
-                    loader: 'babel-loader',
-                    query: {
-                        presets: [
-                            'es2015',
-                            'react'
-                        ]
-                    }
-                },
-                {
-                    test: /\.css$/,
-                    use: [
-                        'style-loader',
-                        'css-loader'
-                    ]
-                }
-            ]
-        }
-    }
+const entryPath = resolve(`${__dirname}/app/src`);
+const outPath = resolve(`${__dirname}/dist`);
+
+// the path(s) that should be cleaned
+let pathsToClean = [
+    'dist'
+]
+
+// the clean options to use
+let cleanOptions = {
+    verbose: true
 }
+
+module.exports = {
+    entry: resolve(`${entryPath}/index.js`),
+    output: {
+        path: outPath,
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: entryPath,
+                loader: 'babel-loader',
+                query: {
+                    presets: [
+                        [
+                            'env',
+                            {
+                                targets: {
+                                    node: '6.11.2',
+                                },
+                            },
+                        ],
+                        'react',
+                    ],
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(pathsToClean, cleanOptions)
+    ]
+};
